12495b9153d706d748e0e734b1d06085e3c06cfe
dhanush <dhanush.10087@gmail.com>
2020-05-23 17:27:58 +0530

    set default timeout to stop wasting time getting info about closed port

--- network_scanner.py
+++ network_scanner.py
@@ -9,7 +9,18 @@ import sys
 import argparse
 
 
+
+
 class Network_Utility:
+	def parse_arguments(self):
+		parser = argparse.ArgumentParser(description="Open Source Network Scanner. Scans for ports, converts domain name to ip address, and vice-versa.")
+		parser.add_argument("ip_value", help="ip address value or domain name for scanning ports and conversion. Examples for ip to domain and vice-versa \"python3 network_scanner 192.168.1.1\", \"python3 network_scanner google.com\"", type=str)
+		parser.add_argument("-p", "--port", default=0, help="Enables port scanner. Specify port or range of port value along with it. \
+Examples \"python3 network_scanner -p 80 google.com\", \
+	\"python3 network_scanner -p 20-80 127.0.0.1\"")
+		args = parser.parse_args()
+		return args
+		
 	def find_ports(self, ports):
 		if ports.find('-') == -1:
 			return -1, int(ports)
@@ -21,6 +32,10 @@ class Network_Utility:
 			self.ip_to_domain(value)
 		else:
 			self.domain_to_ip(value)
+	
+	def create_socket(self, domain_name, port):
+		socket.setdefaulttimeout(1)
+		return socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex((domain_name, port))
 			
 			
 	def domain_to_ip(self, domain_name):
@@ -40,28 +55,16 @@ class Network_Utility:
 			print("Given wrong domain or ip address")
 			sys.exit()
 			
-	def scan_ports(self, ports, domain_name):
-		try:
-			startPort, endPort = self.find_ports(ports)
-			if startPort == -1:
-				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				socket.setdefaulttimeout(1)
-				result = s.connect_ex((domain_name, endPort))
-				if result == 0:
-					print("Port {} is open".format(endPort))
-					s.close
-				else:
-					print("Port {} is closed".format(endPort))
-			else:
+		
 				
-				for port in range(startPort, endPort+1):
-					s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					socket.setdefaulttimeout(1)
-					result = s.connect_ex((domain_name, port))
-					if result == 0:
-						print("Port {} is open".format(port))
-					else:
-						print("Port {} is closed".format(port))
+	def scan_ports_multiprocessing(self, port, domain_name):
+		try:		
+			result = self.create_socket(domain_name, port)
+			if result == 0:
+				print("Port {} is open".format(port))
+			else:
+				print("Port {} is closed".format(port))			
+					
 		except KeyboardInterrupt:
 			print("\nExiting Program")
 			sys.exit()
@@ -71,20 +74,30 @@ class Network_Utility:
 
 
 
-parser = argparse.ArgumentParser(description="Open Source Network Scanner. Scans for ports, converts domain name to ip address, and vice-versa.")
-parser.add_argument("ip_value", help="ip address value or domain name for scanning ports and conversion. Examples for ip to domain and vice-versa \"python3 network_scanner 192.168.1.1\", \"python3 network_scanner google.com\"", type=str)
-parser.add_argument("-p", "--port", default=0, help="Enables port scanner. Specify port or range of port value along with it. \
-Examples \"python3 network_scanner -p 80 google.com\", \
-	\"python3 network_scanner -p 20-80 127.0.0.1\"")
-args = parser.parse_args()
-obj = Network_Utility();
-if args.port:
-	obj.scan_ports(args.port, sys.argv[3])
-	
-else:
-	obj.find_ip_or_domain(sys.argv[1])
+
+if __name__ == "__main__":
+	obj = Network_Utility()
+	args = obj.parse_arguments()
+	if args.port:
+		startPort, endPort = obj.find_ports(args.port)
+		if startPort == -1:
+			obj.scan_ports_multiprocessing(endPort, sys.argv[3])
+		else:
+			for port in range(startPort, endPort+1):
+				obj.scan_ports_multiprocessing(port, sys.argv[3])
+
+							
+	else:
+		obj.find_ip_or_domain(sys.argv[1])
 
 
 
 
+
+
+
+"""
+without multiprocessing, checking 0-100 ports on 192.168.1.8, time taken is 0.0180
+
+"""
 
